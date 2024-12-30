f8231aabf088deb69e41a2b9bebd17ca1bbb8632
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2024-12-30 21:14:03 +0600

    contest

---
+++ XPSC/Week10/vj_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+
+    if(n==7) cout << "THALA" << '\n';
+    else cout << "SADGE" << '\n';
+
+    return 0;
+}
