8c7afbdbc4739fcc1f19c2e6c1479a1cd8bb7bd7
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2025-01-21 22:15:01 +0600

    contest

---
+++ XPSC/Week13/vj_1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int x, y;
+    cin >> x >> y;
+    cout << x * y << '\n';
+
+    return 0;
+}
---
+++ XPSC/Week13/vj_2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << n/4 + (n%4 > 0) << endl;
+    }
+
+    return 0;
+}
